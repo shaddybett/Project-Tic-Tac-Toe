6740cc99501c755e22408d0cc40783f36fa1129e
Shaddy <shadybett540@gmail.com>
2024-01-24 12:56:02 +0300

    done with migration

---
+++ seed.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from models import db, Score, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project-tic-tac.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    new_user = {
+        'username': 'Tim',
+        'password': 123665
+    }
+    new_score = {
+        'scores': 2
+    }
+    user = User(**new_user)
+    score = Score(**new_score)
+
+    with db.session.begin():
+        db.session.add(user)
+        db.session.add(score)
+        db.session.commit()
