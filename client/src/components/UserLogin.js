52a80837fea20c3977a4d973cc3c7083afb74c95
tray <onesimus.githua@student.moringaschool.com>
2024-01-27 17:15:44 +0300

    styled login btn

--- client/src/components/UserLogin.js
+++ client/src/components/UserLogin.js
@@ -65,15 +65,20 @@ function LoginForm() {
                     <p className="lead fw-normal mb-0 me-3">Log Into Your Account</p>
                   </div>
  
-                  <div className="form-outline mb-4">
+                  <div className="form-outline mb-4 form-group">
                     <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="form3Example3" className="form-control form-control-lg" placeholder="Enter a valid email address" />
                     <label className="form-label" for="form3Example3">Email address</label>
                   </div>
  
              
-                  <div className="form-outline mb-3">
+                  <div className="form-outline mb-3 form-group">
                     <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} id="form3Example4" className="form-control form-control-lg" placeholder="Enter password" />
                     <label className="form-label" for="form3Example4">Password</label>
+                  </div>
+
+                  <div className="text-center text-lg-start mt-4 pt-2 form-group2">
+                    <button type="submit" id="login-btn"  >Login</button>
+                    <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="/register" className="link-danger">Register</a></p>
                   </div>
  
                   <div className="d-flex justify-content-between align-items-center">
@@ -93,10 +98,7 @@ function LoginForm() {
           ))
         : null}
  
-                  <div className="text-center text-lg-start mt-4 pt-2">
-                    <button type="submit" className="btn btn-primary btn-lg"  >Login</button>
-                    <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="/register" className="link-danger">Register</a></p>
-                  </div>
+                  
  
                 </form>
               </div>
